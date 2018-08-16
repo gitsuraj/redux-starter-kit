12e94721eb1b582f39d8746b3b4a7cd4ea55fd1d
Suraj Maharjan <surajmaharjan@MacBook-Pro-3.local>
2018-08-16 10:55:47 +1000

    Initialize

---
+++ src/constants/firebase.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyAwmjQIVBGohzR467vUCYcSaGwABXlvXzc',
+  authDomain: 'passwordremember-32812.firebaseapp.com',
+  databaseURL: 'https://passwordremember-32812.firebaseio.com',
+  projectId: 'passwordremember-32812',
+  storageBucket: 'passwordremember-32812.appspot.com',
+  messagingSenderId: '1067100977986',
+};
